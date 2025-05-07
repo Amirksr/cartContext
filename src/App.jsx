bcb075c9a052478574a8bc1a16e087caaf7f51ff
amir kasraeian <amirkasraeian611@gmail.com>
2025-05-07 09:01:12 +0330

    Project without state management

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import CartPage from "./pages/cart/CartPage";
+import { useState } from "react";
+function App() {
+  const [addedToCart , setAddedToCart] = useState(0)
+  const [addedProducts , setAddedProducts] = useState([])
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              addedToCart={addedToCart} 
+              setAddedToCart={setAddedToCart}
+              addedProducts={addedProducts}
+              setAddedProducts={setAddedProducts}
+           />
+          } 
+        />
+        <Route 
+        path="/cart" 
+        element={
+          <CartPage
+            addedToCart={addedToCart}
+            setAddedToCart={setAddedToCart}
+            addedProducts={addedProducts}
+            setAddedProducts={setAddedProducts}
+          />} 
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./Navbar.css";
+import { BsCart4 } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+function Navbar({addedToCart, setAddedToCart}) {
+    
+  return (
+    <nav>
+      <p>
+        <Link to='/'>home</Link>
+      </p>
+      <p className="bag">
+        <Link to='/cart'><BsCart4 color="white" size="30px" /></Link>
+        {addedToCart > 0 && <span>{addedToCart}</span>}
+      </p>
+    </nav>
+  );
+}
+
+export default Navbar;
